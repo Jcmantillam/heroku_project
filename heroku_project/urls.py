0d68e9180748b09ff18825bd47bddb514c497e23
camilo <jcmantillam@unal.edu.co>
2019-08-28 05:27:40 -0500

    front demo de heroku app

--- heroku_project/urls.py
+++ heroku_project/urls.py
@@ -20,5 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$',views.index,name="index")
+    url(r'^$',views.index,name="index"),
+    url(r'/gallery',views.gallery,name="gallery"),
+    url(r'/game',views.game,name="game"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- init/views.py
+++ init/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'init/index.html')
 
+def gallery(request):
+    return render(request,'init/gallery.html')
+
+def game(request):
+    return render(request,'init/game.html')
 
